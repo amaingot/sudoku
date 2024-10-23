fda51640474a76bcdc7889d5fb1eabfdda49ab1e
Alex Maingot <alex.maingot@gmail.com>
2024-10-22 21:39:06 -0500

    feat: Add Terraform Apply workflow
    Add a new GitHub Actions workflow file, terraform-apply.yml, to handle the Terraform apply process. This workflow is triggered on pushes to the main branch or when a tag matching the pattern "v*.*.*" is pushed. It includes steps to determine the environment, configure AWS credentials, set up Terraform, initialize Terraform, and apply the Terraform changes. The workflow runs on the latest version of Ubuntu.
    
    Related to #123

--- terraform/github.tf
+++ terraform/github.tf
@@ -3,6 +3,55 @@ resource "github_repository_environment" "repo_environment" {
   environment = var.environment
 }
 
+resource "github_actions_environment_variable" "lambda_layer_name" {
+  repository    = data.github_repository.repo.name
+  environment   = github_repository_environment.repo_environment.environment
+  variable_name = "LAMBDA_LAYER_NAME"
+  value         = aws_lambda_layer_version.layer.layer_name
+}
+
+resource "github_actions_environment_variable" "api_lambda_name" {
+  repository    = data.github_repository.repo.name
+  environment   = github_repository_environment.repo_environment.environment
+  variable_name = "API_LAMBDA_NAME"
+  value         = aws_lambda_function.api.function_name
+}
+
+resource "github_actions_environment_variable" "ws_lambda_name" {
+  repository    = data.github_repository.repo.name
+  environment   = github_repository_environment.repo_environment.environment
+  variable_name = "WS_LAMBDA_NAME"
+  value         = aws_lambda_function.ws.function_name
+}
+
+resource "github_actions_environment_variable" "auth_lambda_name" {
+  repository    = data.github_repository.repo.name
+  environment   = github_repository_environment.repo_environment.environment
+  variable_name = "AUTH_LAMBDA_NAME"
+  value         = aws_lambda_function.auth.function_name
+}
+
+resource "github_actions_environment_variable" "ui_s3_bucket" {
+  repository    = data.github_repository.repo.name
+  environment   = github_repository_environment.repo_environment.environment
+  variable_name = "UI_S3_BUCKET"
+  value         = aws_s3_bucket.ui.bucket
+}
+
+resource "github_actions_environment_variable" "cloudfront_distribution_id" {
+  repository    = data.github_repository.repo.name
+  environment   = github_repository_environment.repo_environment.environment
+  variable_name = "CLOUDFRONT_DISTRIBUTION_ID"
+  value         = aws_cloudfront_distribution.ui.id
+}
+
+resource "github_actions_environment_variable" "ui_domain" {
+  repository    = data.github_repository.repo.name
+  environment   = github_repository_environment.repo_environment.environment
+  variable_name = "UI_DOMAIN"
+  value         = local.ui_domain
+}
+
 resource "github_actions_environment_variable" "api_domain" {
   repository    = data.github_repository.repo.name
   environment   = github_repository_environment.repo_environment.environment
